ad112711a9952f3ddf92f5384c7a5d6eeb163652
Freekiehsoes <freekattema@live.nl>
2023-10-09 14:09:08 +0200

    yeet

--- input.go
+++ input.go
@@ -1,50 +1,50 @@
 package input
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
-
 	"github.com/ProductionPanic/go-pretty"
-	"github.com/eiannone/keyboard"
 )
 
-func SelectFromList(prompt string, list []string) int {
-	// open a dev/tty file
-	var selected int
-	cursor := GetCursor()
-	cursor.Hide()
-	defer cursor.Show()
-	for {
-		pretty.Println(prompt)
-		for i, item := range list {
-			cursor.ClearLine()
-			if i == selected {
-				pretty.Println("[green,bold]>" + item)
-			} else {
-				pretty.Println(" " + item)
-			}
-			pretty.Print("[reset]")
-		}
-		_, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			panic(err)
-		}
-		if key == keyboard.KeyArrowUp {
-			if selected > 0 {
-				selected--
-			}
-		} else if key == keyboard.KeyArrowDown {
-			if selected < len(list)-1 {
-				selected++
-			}
-		} else if key == keyboard.KeyEnter {
-			break
-		}
-		// move up and clear line
-		linesUp := len(list) + 1
-		cursor.Up(linesUp)
+type TextInput struct {
+	Prompt  string
+	Default string
+}
+
+func GetText(prompt string) string {
+	return NewTextInput(prompt).Run()
+}
+
+func GetInt(prompt string) int {
+	return NewTextInput(prompt).RunInt()
+}
+
+func GetFloat(prompt string) float64 {
+	return float64(NewTextInput(prompt).RunInt())
+}
+
+func NewTextInput(prompt string) *TextInput {
+	return &TextInput{
+		Prompt: prompt,
 	}
-	return selected
+}
+
+func (t *TextInput) Run() string {
+	var output string
+	pretty.Println(t.Prompt)
+	fmt.Scanln(&output)
+	return output
+}
+
+func (t *TextInput) RunInt() int {
+	output := t.Run()
+	var outInt int
+	fmt.Sscanf(output, "%d", &outInt)
+	return outInt
+}
+
+func (t *TextInput) RunFloat() float64 {
+	output := t.Run()
+	var outFloat float64
+	fmt.Sscanf(output, "%f", &outFloat)
+	return outFloat
 }
